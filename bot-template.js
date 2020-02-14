441337d3b375fdbc00d083c344aa2f70325e7d97
retroverse <ebreakey@gmail.com>
2020-02-14 16:36:43 +1100

    Remove redundant functions in bot template

--- bot-template.js
+++ bot-template.js
@@ -7,50 +7,18 @@ const [
   enemiesS = ''
 ] = process.argv
 
-// Create grid (easier for writing bot given imperative stuff idk) (+1 as haskell coords start at 1,1)
-let grid = []
-for (let i = 0; i < 11; i++) {
-  grid[i] = []
-  for (let j = 0; j < 11; j++) {
-    grid[i][j] = {x: i + 1, y: j + 1, team: 'neutral'}
-  }
-}
-
 // Convert friendly and enemy strings to arrays
 const friendlies = friendliesS.length !== 0 ? friendliesS.split('|').map(unit => unit.split(',').map(num => Number(num))) : []
 const enemies = enemiesS.length !== 0 ? enemiesS.split('|').map(unit => unit.split(',').map(num => Number(num))) : []
 
-// Add units to grid representation
-for (let friendly of friendlies) {
-  let [x, y] = friendly
-  grid[x - 1][y - 1].team = 'friendly'
-}
-
-for (let enemy of enemies) {
-  let [x, y] = enemy
-  grid[x - 1][y - 1].team = 'enemy'
-}
-
-// some helpers (prob only temporarily located here)
-const getAllCheckers = (grid) => {
-  let checkers = []
-  for (let row of grid) {
-    for (let checker of row) {
-      checkers.push(checker)
-    }
-  }
-
-  return checkers
-}
-
-function bot (grid, friendlies, enemies) {
+function bot (friendlies, enemies) {
  /* BOT-START */
 
  /* BOT-END */
 }
 
 // Get output from Bot
-const {x, y} = bot(grid, friendlies, enemies)
+const {x, y} = bot(friendlies, enemies)
 
 // Output to STDOUT
 process.stdout.write(`${x},${y}`)
