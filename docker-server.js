7afbb8a686274eda5af88dfdd651e4d122bf5685
retroverse <ebreakey@gmail.com>
2020-02-14 16:37:00 +1100

    Docker server accepts code from body

--- docker-server.js
+++ docker-server.js
@@ -10,52 +10,64 @@ const VERBOSE = false
 
 const server = http.createServer((req, res) => {
   if (VERBOSE) { console.log('Incoming query...') }
-  // Parse query
-  let query
-  try {
-    const queryString = url.parse(req.url).query
-    query = qstring.parse(queryString)
-  } catch (err) {
-    console.error('Error parsing query string', err)
-    writeError(res, 500, 'Error parsing query string', err)
-    return
-  }
+  
+  let body = []
+  req.on('error', err => {
+    console.errror(err)
+  }).on('data', chunk => {
+    body.push(chunk)
+  }).on('end', () => {
+    // Concatenate chunks into a string
+    body = Buffer.concat(body).toString()
 
-  // Pull query params
-  if (!(query.redCode && query.blueCode)) {
-    console.error('Query must include "redCode" and "blueCode" parameters')
-    writeError(res, 400, 'Query must include "redCode" and "blueCode" parameters')
-    return
-  }
-  const {redCode, blueCode} = query
-
-  // Escape quote characters
-  const escapedRedCode = redCode.replace(/(["\\])/g, '\\$1')
-  const escapedBlueCode = blueCode.replace(/(["\\])/g, '\\$1')
-
-  // Spawn Process
-  if (VERBOSE) { console.log(`Spawning process for game between "${redCode.slice(0, 8)}..." and "${blueCode.slice(0, 8)}..."`) }
-  exec(`${COMMAND} "${escapedRedCode}" "${escapedBlueCode}"`, (err, stdout, stderr) => {
-    // Was there an error?
-    if (err) {
-      writeError(res, 500, 'Error spawning hexskell process', err)
+    // Must have a body provided
+    if (!body) {
+      console.error('Request must include a body')
+      writeError(res, 400, 'Request must include a body')
       return
     }
 
-    // Parse output
-    let results
+    // Parse the body
+    let data
     try {
-      results = JSON.parse(stdout)
-    } catch (err) {
-      writeError(res, 500, `Error parsing child process JSON: "${stdout}"`, err)
+      data = JSON.parse(body)
+    } catch (error) {
+      console.error(error)
+      writeError(res, 500, 'Error parsing body JSON')
       return
     }
 
-    // Respond with output
-    if (VERBOSE) { console.log('Game execution complete') }
-    res.writeHead(200, { 'Content-Type': 'text/json' })
-    res.write(JSON.stringify(results))
-    res.end()
+    // Get code from data
+    let { redCode, blueCode } = data
+
+    // Escape the code's quote chars
+    const escapedRedCode = redCode.replace(/(["\\])/g, '\\$1')
+    const escapedBlueCode = blueCode.replace(/(["\\])/g, '\\$1')
+
+    // Spawn Process
+    if (VERBOSE) { console.log(`Spawning process for game between "${redCode.slice(0, 8)}..." and "${blueCode.slice(0, 8)}..."`) }
+    exec(`${COMMAND} "${escapedRedCode}" "${escapedBlueCode}"`, (err, stdout, stderr) => {
+      // Was there an error?
+      if (err) {
+        writeError(res, 500, 'Error spawning hexskell process', err)
+        return
+      }
+
+      // Parse output
+      let results
+      try {
+        results = JSON.parse(stdout)
+      } catch (err) {
+        writeError(res, 500, `Error parsing child process JSON: "${stdout}"`, err)
+        return
+      }
+
+      // Respond with output
+      if (VERBOSE) { console.log('Game execution complete') }
+      res.writeHead(200, { 'Content-Type': 'text/json' })
+      res.write(JSON.stringify(results))
+      res.end()
+    })
   })
 })
 
